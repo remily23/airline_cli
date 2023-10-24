f499e337ac5afc73467e030b4536fc4da747c849
Faiz Fazaluddin <faiz.mf07@gmail.com>
2023-10-24 11:59:24 +0100

    passenger G and S name tests

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,2 +1,85 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class PassengerTest {
-}
+    Passenger passenger;
+
+    @BeforeEach
+    public void setUp(){
+        passenger= new Passenger(
+                "Spongebob",
+                12345678,
+                25,
+                87654321,
+                PassengerType.ECONOMY,
+                23);
+    }
+
+
+    @Test
+    public void canGetName(){
+        assertThat(passenger.getName()).isEqualTo("Spongebob");
+    }
+    @Test
+    public void canSetName(){
+        passenger.setName("Squidward");
+        assertThat(passenger.getName()).isEqualTo("Squidward");
+    }
+    @Test
+    public void canGetPassportNumber(){
+        assertThat(passenger.getPassportNumber()).isEqualTo(12345678);
+    }
+//    @Test
+//    public void canSetName(){
+//        passenger.setName("Squidward");
+//        assertThat(passenger.getName()).isEqualTo("Squidward");
+//    }
+//    @Test
+//    public void canGetName(){
+//        assertThat(passenger.getName()).isEqualTo("Spongebob");
+//    }
+//    @Test
+//    public void canSetName(){
+//        passenger.setName("Squidward");
+//        assertThat(passenger.getName()).isEqualTo("Squidward");
+//    }
+//    @Test
+//    public void canGetName(){
+//        assertThat(passenger.getName()).isEqualTo("Spongebob");
+//    }
+//    @Test
+//    public void canSetName(){
+//        passenger.setName("Squidward");
+//        assertThat(passenger.getName()).isEqualTo("Squidward");
+//    }
+//    @Test
+//    public void canGetName(){
+//        assertThat(passenger.getName()).isEqualTo("Spongebob");
+//    }
+//    @Test
+//    public void canSetName(){
+//        passenger.setName("Squidward");
+//        assertThat(passenger.getName()).isEqualTo("Squidward");
+//    }
+//    @Test
+//    public void canGetName(){
+//        assertThat(passenger.getName()).isEqualTo("Spongebob");
+//    }
+//    @Test
+//    public void canSetName(){
+//        passenger.setName("Squidward");
+//        assertThat(passenger.getName()).isEqualTo("Squidward");
+//    }
+//    @Test
+//    public void canGetName(){
+//        assertThat(passenger.getName()).isEqualTo("Spongebob");
+//    }
+//    @Test
+//    public void canSetName(){
+//        passenger.setName("Squidward");
+//        assertThat(passenger.getName()).isEqualTo("Squidward");
+//    }
+//
+//}
