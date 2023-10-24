0bb4670cd320fc8868b17a4e97b1ae10d544be84
Emily Reid <emily.reid31@gmail.com>
2023-10-24 11:44:30 +0100

    completed passenger class and passenger enum

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,5 +1,79 @@
 public class Passenger {
 
-    
+    private String name;
+    private long passportNumber;
+    private int age;
+    private long phoneNumber;
+    private PassengerType passengerType;
+    private int luggageAllowance;
 
+    public Passenger(String name, long passportNumber, int age, long phoneNumber, PassengerType passengerType, int luggageAllowance) {
+        this.name = name;
+        this.passportNumber = passportNumber;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.passengerType = passengerType;
+        this.luggageAllowance = luggageAllowance;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPassportNumber() {
+        return this.passportNumber;
+    }
+
+    public void setPassportNumber(long passportNumber) {
+        this.passportNumber = passportNumber;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public long getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getLuggageAllowance() {
+        return this.luggageAllowance;
+    }
+
+    public void setLuggageAllowance(int luggageAllowance) {
+        this.luggageAllowance = luggageAllowance;
+    }
+
+    public void addLuggage(int luggage){
+        this.luggageAllowance += luggage;
+
+    }
+    public void removeLuggage(int luggage){
+        this.luggageAllowance -= luggage;
+    }
+
+    public PassengerType getPassengerType() {
+        return passengerType;
+    }
+
+    public void setPassengerType(PassengerType passengerType) {
+        this.passengerType = passengerType;
+    }
+
+    public void upgradePassenger(PassengerType passengerType){
+        this.setPassengerType(passengerType);
+    }
+    
 }
---
+++ src/main/java/PassengerType.java
@@ -0,0 +1,6 @@
+public enum PassengerType {
+    ECONOMY,
+    BUSINESS,
+    FIRST;
+
+}
