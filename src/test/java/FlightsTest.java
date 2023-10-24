7211a22576d161b1bc693a1d5d90eba9fafbbdef
Emily Reid <emily.reid31@gmail.com>
2023-10-24 14:58:54 +0100

    added flights tests

--- src/test/java/FlightsTest.java
+++ src/test/java/FlightsTest.java
@@ -1,3 +1,32 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class FlightsTest {
+    Flights flights;
+    Flight flight;
+
+
+    @BeforeEach
+    public void setUp(){
+        flights = new Flights();
+        flight = new Flight(1051, Location.HEATHROW, Location.PARIS, LocalDate.of(2023,10,24), LocalDate.of(2023, 10,31), 1.5);
+    }
+
+    @Test
+    public void canAddFlight(){
+        flights.addFlight(flight);
+        assertThat(flights.getFlights().size()).isEqualTo(1);
+    }
+
+    @Test
+    public void canRemoveFlight(){
+        flights.addFlight(flight);
+        flights.cancelFlight(flight);
+        assertThat(flights.getFlights().size()).isEqualTo(0);
+    }
 
 }
